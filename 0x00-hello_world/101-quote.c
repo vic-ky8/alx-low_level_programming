d63d617a89389c7fce335c7360234cbb4f6edfda
victoria tom <mbatvictoria@gmail.com>
2023-04-21 05:22:23 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - Entry point
+ *
+ * Description: print a quote using write function
+ * ssize_t write(int fd, const void *buf, size_t count)
+ * Return: 1 (error)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return (1);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1, quo, 59);
+	return (1);
 }
