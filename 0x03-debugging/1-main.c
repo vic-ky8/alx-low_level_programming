df86b747091a94db05b65a3649c103f3745d12d6
victoria tom <mbatvictoria@gmail.com>
2023-04-28 00:13:11 -0700

    commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -5,8 +5,12 @@
 * Return: 0
 */
 
+
+
 int main(void)
+
 {
+
 	int i;
 
 	printf("Infinite loop incoming :(\n");
@@ -18,4 +22,3 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
-
