5713e4be38b94516d903756358dd9a4d84a202d9
victoria tom <mbatvictoria@gmail.com>
2023-04-28 09:28:31 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - A function that print number from 0 to 9
+ * Return: Always (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar("&c", i)
+	}
+	_putchar('\n');
+}
