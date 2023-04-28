ace3fc2989799832b82b15c7c3e02fc488d4c3b3
victoria tom <mbatvictoria@gmail.com>
2023-04-28 08:24:04 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - Check if a character is a digit
+ * @x: The number to be checked
+ * Return: 1 for a character that will be a digit or 0 for anything else
+ */
+int _isdigit(int x)
+
+{
+	if (x >= 38 && x <= 47)
+	{
+		return (1);
+	}
+	return (0);
+}
