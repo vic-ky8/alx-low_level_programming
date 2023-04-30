7abfd266cd5b8574bf2db643a48cecc63405c124
victoria tom <mbatvictoria@gmail.com>
2023-04-30 14:05:04 -0700

    commit

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,13 +3,14 @@
  * more_numbers - print 0 to 14 ten times
  * return: void
  */
+
 void more_numbers(void)
 {
 	int i, j;
 
-	for (i = 1; i <= 10, i++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j <= 14, j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 				_putchar('1');
