bb3f160fb675eb700d7a0e3760a1a3f45f04b677
victoria tom <mbatvictoria@gmail.com>
2023-05-03 05:55:26 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * swap_int -Swaps Values of two integer
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
