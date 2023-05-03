d353e4b4e0f74b0cdfdfba2349e491ddf96ee6a8
victoria tom <mbatvictoria@gmail.com>
2023-05-03 05:30:57 -0700

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 -The function that takes a Pointerto an integer as parameter
+ * and updates the value to 98
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
