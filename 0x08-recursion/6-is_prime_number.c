c32797ad6be6c86cfe51cac6730a792d1527ddad
victoria tom <mbatvictoria@gmail.com>
2023-05-17 05:26:44 -0700

    commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * is_divisible - Checks if a number is divisible
- * @num: The number to be checked
- * @div: The divisor
+ * actual_prime - Checks if a number is prime recursively
+ * @n: The number to be checked
+ * @i: The iterator
  * Return: If the number is divisible - 0
  * If the number is not divisible - 1
  */
-int is_divisible(int num, int div)
+int actual_prime(int n, int i)
 {
-	if (div == 1)
+	if (i == 1)
 		return (1);
-	if (num % div == 0 && div > 0)
+	if (n % i == 0 && i > 0)
 		return (0);
-	return (is_divisible(num, div - 1));
+	return (actual_prime(n, i - 1));
 }
 
 /**
@@ -25,5 +25,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (is_prime_number(n, num - 1));
+	return (actual_prime(n, n - 1));
 }
