35fd389ac1cc4bfe696d5363d4ed5a5f35e93b15
vic-ky8 <117303953+vic-ky8@users.noreply.github.com>
2023-07-24 15:29:30 +0100

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,21 @@
  *       */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-		int fd;
-			ssize_t nrd, nwr;
-				char *buf;
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-					if (!filename)
-								return (0);
-
-						fd = open(filename, O_RDONLY);
-
-							if (fd == -1)
-										return (0);
-
-								buf = malloc(sizeof(char) * (letters));
-									if (!buf)
-												return (0);
-
-										nrd = read(fd, buf, letters);
-											nwr = write(STDOUT_FILENO, buf, nrd);
-
-												close(fd);
-
-													free(buf);
-
-														return (nwr);
+	if (!filename)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+	close(fd);
+	free(buf);
+	return (nwr);
 }
